94f822aa8c59c19f11d0313fbae97e6e4870946a
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-06-19 15:09:58 +0300

    create a user profileform

--- Neighbor/forms.py
+++ Neighbor/forms.py
@@ -7,3 +7,8 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email','password1', 'password2']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'bio', 'avatar', 'neighborhood']
