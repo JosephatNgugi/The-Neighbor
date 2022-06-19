65e1dd9025500527a4de2ce8c2edd14906b3c0a5
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-06-19 14:59:22 +0300

    create a user registration method and add logout and login urls

--- Neighbor/urls.py
+++ Neighbor/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path('', views.homepage, name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/sign-up/', views.UserRegistration, name='signup'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- Neighbor/views.py
+++ Neighbor/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 
+from .forms import SignUpForm
 from .models import *
 
 # Create your views here.
@@ -8,3 +11,17 @@ def homepage(request):
     all_posts = Post.objects.all()
     posts = all_posts[::-1]
     return render(request, 'neighbor/home.html',{'posts':posts})
+
+def UserRegistration(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            InputPassword = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=InputPassword)
+            login(request, user)
+            return redirect('homepage')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/registration_form.html', {'form': form})
